be2203d72e23f815897a4caa4df9a95f03393f51
William Kim <scarf3blue@gmail.com>
2017-01-21 14:14:58 -0500

    Implement login.

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@ var assert = require('assert');
 var express = require('express');
 var router = express.Router();
 
+// Connection URL
+var url = 'mongodb://localhost:27017/devsideprojects';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("Entered /");
-  // Connection URL
-  var url = 'mongodb://localhost:27017/devsideprojects';
+	res.render('index');
 
   // Use connect method to connect to the server
-  MongoClient.connect(url, function(err, db) {
+  /*MongoClient.connect(url, function(err, db) {
   	assert.equal(null, err);
     console.log("Connected successfully to server GET");
 
@@ -21,32 +22,42 @@ router.get('/', function(req, res, next) {
       res.render('index', { name: query_result[0].name });
       db.close();
     });
-  });
+  });*/
 
 });
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  var inputType = req.body.button;
 
-  // Connection URL
-  var url = 'mongodb://localhost:27017/devsideprojects';
   // Use connect method to connect to the server
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server POST");
 
-    insertDocuments(db, req.body, function() {
-      res.render('index', { name: req.body.name });
+    findUser(db, { username: req.body.username }, function(docs) {
+      res.render('index');
+      if (docs.length != 0) {
+      	findUser(db, req.body, function(docs) {
+      		if (docs.length != 0) {
+      			res.send("Login successful");
+      		} else {
+      			res.send("Incorrect password");
+      		}
+      	})
+      } else {
+      	res.send("User doesn't exist");
+      }
       db.close();
     });
   });
-
 });
 
-var findDocuments = function(db, query, callback) {
+var findUser = function(db, query, callback) {
   // Get the documents collection
   var collection = db.collection('users');
   // Find some documents
+  console.log(query);
   collection.find(query).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
@@ -58,10 +69,8 @@ var findDocuments = function(db, query, callback) {
 var insertDocuments = function(db, reqbody, callback) {
   // Get the documents collection
   var collection = db.collection('users');
-  console.log(reqbody);
   // Insert some documents
-  collection.save(reqbody,
-    function(err, result) {
+  collection.save(reqbody, function(err, result) {
     assert.equal(err, null);
     console.log("Inserted the document into the collection");
     callback(result);
