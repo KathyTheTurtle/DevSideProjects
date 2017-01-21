952bcc536781d02ac91ec95c7ed9865dedaa0e2c
HAR15 <haris005@gmail.com>
2017-01-21 00:45:00 -0500

    Mongo module

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/devsideprojects';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
